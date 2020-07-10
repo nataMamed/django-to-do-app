a83e9642148229ac14ff08d6ecf02eeac3715838
Natã Mamede <natamamede@gmail.com>
2020-07-10 10:29:00 -0300

    feat: creating the  task model and registering

--- src/tasks/admin.py
+++ src/tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+class ListingTasks(admin.ModelAdmin):
+
+    list_display       = ('id', 'title', 'date_created', 'complete')
+    list_display_links = ('id', 'title')
+    search_fields      = ('title',)
+    list_filter        = ('title',)
+    list_per_page      = 5
+
+admin.site.register(Task, ListingTasks)
