742449328bf7116458a8eed3710aa89debd89074
Natã Mamede <natamamede@gmail.com>
2020-07-10 09:38:57 -0300

    feat: adding the templates folder and setting it

--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index_view(request):
 
-    return HttpResponse('Index')
+    return render(request, template_name='tasks/index.html')
